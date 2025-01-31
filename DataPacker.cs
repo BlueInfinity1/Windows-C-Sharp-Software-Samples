fa56369594528950505f957d899daa0174173ed5
BlueInfinity1 <74124180+BlueInfinity1@users.noreply.github.com>
2025-01-31 16:30:44 +0200

    Update DataPacker.cs

--- DataPacker.cs
+++ DataPacker.cs
@@ -1,47 +1,75 @@
-﻿using System.IO.Compression;
+using System.IO.Compression;
 using System.IO;
 using System.Collections.Generic;
 
 namespace NativeService
 {
-    //NOTE: This class is currently unused due to packing and encrypting work happening in the same class
-
-    class DataPacker //methods for dealing with byte[] data, e.g. building packages and compression
+    class DataPacker // Methods for dealing with byte[] data (compression, packaging)
     {
         public static byte[] CompressData(byte[] dataToCompress)
         {
             using (MemoryStream outputStream = new MemoryStream())
             {
-                using (DeflateStream compressionStream = new DeflateStream(outputStream, CompressionLevel.Optimal))
+                using (DeflateStream compressionStream = new DeflateStream(outputStream, CompressionLevel.Optimal, true))
                 {
                     compressionStream.Write(dataToCompress, 0, dataToCompress.Length);
+                    compressionStream.Flush(); // Ensure all data is written
                 }
                 return outputStream.ToArray();
             }
         }
 
-        public static byte[] CompressData(List<FileInfo> filesToCompress)
-        {
-            /*using (MemoryStream outputStream = new MemoryStream())
-            {
-                using (DeflateStream compressionStream = new DeflateStream(outputStream, CompressionLevel.Optimal))
-                {
-                    compressionStream.Write(dataToCompress, 0, dataToCompress.Length);
-                }
-                return outputStream.ToArray();
-            }*/
-            return null;
-        }
-
         public static byte[] DecompressData(byte[] dataToDecompress)
         {
-            return null; //TODO if needed
+            using (MemoryStream inputStream = new MemoryStream(dataToDecompress))
+            {
+                using (MemoryStream outputStream = new MemoryStream())
+                {
+                    using (DeflateStream decompressionStream = new DeflateStream(inputStream, CompressionMode.Decompress))
+                    {
+                        decompressionStream.CopyTo(outputStream);
+                    }
+                    return outputStream.ToArray();
+                }
+            }
+        }
+
+        public static byte[] CompressFiles(List<FileInfo> filesToCompress)
+        {
+            using (MemoryStream outputStream = new MemoryStream())
+            {
+                using (GZipStream compressionStream = new GZipStream(outputStream, CompressionLevel.Optimal, true))
+                {
+                    foreach (var file in filesToCompress)
+                    {
+                        if (!file.Exists) continue;
+
+                        byte[] fileBytes = File.ReadAllBytes(file.FullName);
+                        compressionStream.Write(fileBytes, 0, fileBytes.Length);
+                    }
+                    compressionStream.Flush();
+                }
+                return outputStream.ToArray();
+            }
         }
 
         public static byte[] BuildMultiFileBytePackage(List<FileInfo> files)
         {
-            //TODO: Construct a byte array by appending each file in files to the byte array
-            return null;
+            using (MemoryStream packageStream = new MemoryStream())
+            {
+                using (BinaryWriter writer = new BinaryWriter(packageStream))
+                {
+                    foreach (var file in files)
+                    {
+                        if (!file.Exists) continue;
+
+                        byte[] fileBytes = File.ReadAllBytes(file.FullName);
+                        writer.Write(fileBytes.Length); // Write file size
+                        writer.Write(fileBytes); // Write file data
+                    }
+                }
+                return packageStream.ToArray();
+            }
         }
     }
 }
